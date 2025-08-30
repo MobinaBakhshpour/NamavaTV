48e1c738a084917c88feb36581a147f7b9e94fee
lenovo <Mobinabakhshipour.1380@gmail.com>
2025-08-30 17:02:51 +0330

    add Header

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+const headerElem = document.querySelector('.header')
+let lastScroll = 0;
+
+const showHeaderHandler = () => {
+    let currentScroll = window.pageYOffset;
+
+    if (currentScroll > lastScroll && currentScroll > 20) {
+        headerElem.classList.add('header--hidden')
+    } else {
+        headerElem.classList.remove('header--hidden')
+    }
+    lastScroll = currentScroll
+}
+
+window.addEventListener('scroll', showHeaderHandler)
