6040b32e054a6b762932c430ad0139418f5aaa20
lenovo <Mobinabakhshipour.1380@gmail.com>
2025-09-03 14:09:09 +0330

    add introduction section => watchOnline & add config section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,41 @@
-//up/down header
+//show or hide header
+function initHeader() {
+    const headerElem = document.querySelector('.header')
+    let lastScroll = 0;
 
-const headerElem = document.querySelector('.header')
-let lastScroll = 0;
+    window.addEventListener('scroll', () => {
+        let currentScroll = window.pageYOffset;
 
-const showHeaderHandler = () => {
-    let currentScroll = window.pageYOffset;
-
-    if (currentScroll > lastScroll && currentScroll > 20) {
-        headerElem.classList.add('header--hidden')
-    } else {
-        headerElem.classList.remove('header--hidden')
-    }
-    lastScroll = currentScroll
+        if (currentScroll > lastScroll && currentScroll > 20) {
+            headerElem.classList.add('header--hidden')
+        } else {
+            headerElem.classList.remove('header--hidden')
+        }
+        lastScroll = currentScroll
+    })
 }
 
-window.addEventListener('scroll', showHeaderHandler)
+// show more or less of the config__features
+function initMoreBtn() {
+    const moreBtn = document.querySelector(".config__features__moreBtn")
+    const cols = document.querySelectorAll(".col2, .col3, .col4");
+    const titleMoreBtn = document.querySelector(".config__features__moreBtn span");
+    const svgMoreBtn = document.querySelector(".config__features__moreBtn svg");
 
+    moreBtn.addEventListener("click", () => {
+        const isOpen = svgMoreBtn.classList.toggle("active")
+        cols.forEach(elm => {
+            elm.classList.toggle('is-open')
+        })
+        titleMoreBtn.innerHTML = isOpen ? "اطلاعات کمتر" : "اطلاعات بیشتر";
+
+    })
+}
+
+
+function initApp() {
+    initHeader()
+    initMoreBtn()
+}
+
+document.addEventListener("DOMContentLoaded", initApp)
