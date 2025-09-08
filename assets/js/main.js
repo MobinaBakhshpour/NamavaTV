6fab57e26da288445baaa3e24759323869624e3c
Mobina <Mobinabakhshipour.1380@gmail.com>
2025-09-08 15:30:37 +0330

    rename functions & reset active class on accordion svg when another item opens

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 //show or hide header
-function initHeader() {
+function setupHeader() {
     const headerElem = document.querySelector('.header')
     let lastScroll = 0;
 
@@ -16,7 +16,7 @@ function initHeader() {
 }
 
 //showing numbers in Persian
-function toPersianDigits(str) {
+function convertNumbersToPersian(str) {
     const selectors = "svg,script,style,code,pre,textarea,input,select,[contenteditable]";
     const toFarsi = num => num.replace(/\d/g, d => '۰۱۲۳۴۵۶۷۸۹'[d]);
     document.querySelectorAll("*:not(" + selectors + ")").forEach(el => {
@@ -25,7 +25,7 @@ function toPersianDigits(str) {
 }
 
 // show more or less of the config__features
-function initMoreBtn() {
+function handelMoreBtnClick() {
     const moreBtn = document.querySelector(".config__features__moreBtn")
     const cols = document.querySelectorAll(".col2, .col3, .col4");
     const titleMoreBtn = document.querySelector(".config__features__moreBtn span");
@@ -41,7 +41,7 @@ function initMoreBtn() {
 }
 
 // manage dropdown list of question
-function initAccordion() {
+function handelAccordion() {
     const questionElms = document.querySelectorAll(".questions__box__item__question")
     const answerElms = document.querySelectorAll('.questions__box__item__answer')
 
@@ -51,6 +51,7 @@ function initAccordion() {
             answerElms.forEach(answer => {
                 if (answer !== event.currentTarget.nextElementSibling) {
                     answer.classList.remove("show")
+                    answer.previousElementSibling.children[1].classList.remove("active")
                 }
             })
             event.currentTarget.nextElementSibling.classList.toggle('show')
@@ -60,10 +61,10 @@ function initAccordion() {
 }
 
 function initApp() {
-    initHeader()
-    toPersianDigits()
-    initMoreBtn()
-    initAccordion()
+    setupHeader()
+    convertNumbersToPersian()
+    handelMoreBtnClick()
+    handelAccordion()
 }
 
 document.addEventListener("DOMContentLoaded", initApp)
